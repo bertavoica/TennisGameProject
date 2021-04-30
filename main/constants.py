9d5e4d7ac65ea809ae7a139b32ef31f0582853e8
elvis2369 <mircea_pantazi@yahoo.com>
2021-04-30 18:35:54 +0300

    created a player image and made him move on the field

--- main/constants.py
+++ main/constants.py
@@ -1,7 +1,7 @@
 # define some colors
 # define the screen
 # define scenes
-
+import  pygame
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RESOLUTION = (700, 700)
@@ -10,3 +10,9 @@ WIDTH = 600
 HEIGHT = 600
 FIRST_SCENE = 0
 SECOND_SCENE = 1
+point_x = 0
+point_y = 0.1
+width = 800
+height = 800
+x=300
+y=300
